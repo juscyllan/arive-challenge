24de70e87bd17d2ddae6e28613b9acb609ab3e48
jsqueiroz <juscyllan@kujhy.com>
2021-07-16 20:01:21 -0300

    add safe area

--- apps/native/src/App.tsx
+++ apps/native/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 
 import HomeScreen from './screens/HomeScreen';
 
-const App = () => {
-  return <HomeScreen />;
-};
+const App = () => (
+  <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+    <HomeScreen />
+  </SafeAreaProvider>
+);
 
 export default App;
--- apps/native/src/screens/HomeScreen.tsx
+++ apps/native/src/screens/HomeScreen.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import CategoryPageHeader from '../components/CategoryPageHeader';
 
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const [currentCategory, setCurrentCategory] = useState(0);
   return (
     <FlatList
-      style={{ marginTop: 30 }}
+      style={{ marginTop: top }}
       data={[1, 2, 3, 4, 5, 6, 7]}
       keyExtractor={i => `${i}`}
       renderItem={() => null}
-      ListHeaderComponent={<CategoryPageHeader title="Personal Care" />}
+      ListHeaderComponent={
+        <>
+          <CategoryPageHeader title="Personal Care" />
+          <CategoryTabList current={currentCategory} />
+        </>
+      }
     />
   );
 };
